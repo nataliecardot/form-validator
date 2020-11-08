8516430c336344d6d4d338d806154abb00505e64
Natalie-Cardot <nataliecardot@gmail.com>
2020-11-08 00:55:55 -0800

    Check validity of email

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ function showSuccess(input) {
 }
 
 // Check that email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 // Check required fields
@@ -65,4 +69,5 @@ form.addEventListener('submit', (e) => {
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
